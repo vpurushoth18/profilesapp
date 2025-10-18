91ac5791685f0e02cc20dbf8d008d8c78cadadfd
Purushoth <vpurushoth97@gmail.com>
2025-10-18 11:21:05 -0700

    displaying user profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { Amplify } from 'aws-amplify';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import App from './App.jsx';
+// import outputs from '../amplify_outputs.json';
+// import './index.css';
+// import '@aws-amplify/ui-react/styles.css';
 
-// src/main.jsx
+// Amplify.configure(outputs);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Authenticator>
+//       <App />
+//     </Authenticator>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
@@ -23,26 +31,13 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <div className="auth-wrap">
+      <Authenticator>
+        <App />
+      </Authenticator>
+    </div>
   </React.StrictMode>
 );
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <div
-//       style={{
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         height: '100vh',
-//         backgroundColor: '#1e1e1e', // same dark background you have
-//       }}
-//     >
-//       <Authenticator>
-//         <App />
-//       </Authenticator>
-//     </div>
-//   </React.StrictMode>
-// );
+
+
 
